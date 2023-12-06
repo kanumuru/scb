acc14b46669da24050372bc45a74ff8cfb28e84b
Kanumuru Rajesh <rajesh@we45.com>
2023-12-06 14:01:45 +0530

    made changes to task 4

--- task4/app.py
+++ task4/app.py
@@ -4,7 +4,7 @@ import sqlite3
 import yaml
 import xml.etree.ElementTree as ET
 import sys
-
+import os
 def parse_csv(csv_file_path):
     with open(csv_file_path, 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -58,42 +58,60 @@ def insert_data_yaml(cursor, data):
     # Inserting YAML data into the table
     for person in data['persons']:
         cursor.execute("INSERT INTO sample_table (id, name, age) VALUES (?, ?, ?)", (person['id'], person['name'], person['age']))
+def list_files_in_directory(directory_path):
+    try:
+        # Get the list of filenames in the specified directory
+        filenames = os.listdir(directory_path)
+
+        # Print the filenames
+        print("Filenames in '{}' directory:".format(directory_path))
+        for filename in filenames:
+            print(filename)
+
+    except FileNotFoundError:
+        print("Directory not found: '{}'".format(directory_path))
+    except PermissionError:
+        print("Permission error: Unable to access '{}' directory.".format(directory_path))
+    except Exception as e:
+        print("An error occurred:", str(e))
 
 def main():
     # Replace 'your_database.db' with the desired SQLite database name
     database_name = 'scb.db'
-
-    filename = sys.argv[1]
-
-    name, extension = filename.split('.')
-    print(name, extension)
-
     # Connect to SQLite database
     conn = sqlite3.connect(database_name)
     cursor = conn.cursor()
     # Create the table if it doesn't exist
     create_table(cursor)
 
-    
-    if extension == 'csv':
-        csv_data = parse_csv(filename)
-        insert_data_csv(cursor, csv_data)
-        
-    elif extension == 'json':
-        json_data = parse_json(filename)
-        insert_data_json(cursor, json_data)
+    filepath = "/Users/kanumururajesh/Desktop/tmp/scb/automation/task4/dataset/"
 
-    elif extension == 'xml':
-        xml_data = parse_xml(filename)
-        insert_data_xml(cursor, xml_data)
+    filenames = os.listdir(filepath)
+    print(filenames)
+    for filename in filenames:
+        print(filename)
+        name, extension = filename.split('.')
+        print(name, extension)
 
-    elif extension == 'yaml' or extension == 'yml':
-        yaml_data = parse_yaml(filename)
-        insert_data_yaml(cursor, yaml_data)
+        if extension == 'csv':
+            csv_data = parse_csv(filepath+filename)
+            insert_data_csv(cursor, csv_data)
+            
+        elif extension == 'json':
+            json_data = parse_json(filename)
+            insert_data_json(cursor, json_data)
 
-    else:
-        print("Invalid file format")
+        elif extension == 'xml':
+            xml_data = parse_xml(filename)
+            insert_data_xml(cursor, xml_data)
 
+        elif extension == 'yaml' or extension == 'yml':
+            yaml_data = parse_yaml(filename)
+            insert_data_yaml(cursor, yaml_data)
+
+        else:
+            print("Invalid file format")
+        os.remove(filepath+filename)
     # Commit changes and close the database connection
     conn.commit()
     conn.close()
